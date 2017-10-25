3a7bd2a4993ebca57e32f0e4c47ab326da76570b
ZephV <zephkwok@outlook.com>
2017-10-25 22:34:01 +0800

    add function:'delete','update'.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 ;(function(){
     'use strict';
 
+    function copy(obj){
+        return Object.assign({},obj);
+    }
+
     new Vue({
         el: '#wrapper',
         data: {
@@ -8,14 +12,39 @@
             current: {}
         },
         methods: {
-            add: function(){
-                var  title = this.current.title;
-                if(!title && title !== 0)return;
-                var todo = Object.assign({},this.current);
-                this.list.push(todo);
+            merge: function(){
+
+                var is_update = this.current.id;
+                if(is_update){
+                    var index = this.list.findIndex(function(item){
+                        return item.id === is_update;
+                    });
+                    Vue.set(this.list,index,copy(this.current));
+                }else{
+                    var  title = this.current.title;
+                    if(!title && title !== 0)return;
+
+                    var todo = copy(this.current);
+                    todo.id = this.next_id();
+                    this.list.push(todo);
+                }
+                this.reset_current();
             },
-            update:function(){},
-            remove:function(){}
+            remove: function(id){
+                this.list.splice(id,1);
+            },
+
+            next_id: function(){
+                return this.list.length + 1;
+            },
+
+            set_current: function(todo){
+                this.current = copy(todo);
+            },
+
+            reset_current: function(){
+                this.set_current({});
+            }
         }
     });
 
